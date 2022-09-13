4e71e1c3b3722fff8816039eee538d9af3196e48
Глеб Бутяев <iiaiias1992@gmail.com>
2022-09-13 15:30:41 +0300

    Нихуя не получилось

--- Среднее число/Program.cs
+++ Среднее число/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using System.Runtime.Serialization.Formatters;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        int[] nums = new int[4];
+        
+        Console.WriteLine("Введите 10 чисел, если необходимо остановить ввод, введите \"0\"");
+        
+        for (int i = 0; i<nums.Length; i++)
+        {
+            
+            nums[i] = int.Parse(Console.ReadLine());
+            if (Console.ReadLine() == "0") break;
+            else if (nums[i] ==4) break;
+
+
+        }
+        
+
     }
 }
